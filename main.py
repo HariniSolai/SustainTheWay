e2e097385ef7c8a946d02dc644c799757c1e40a5
HariniSolai <harisolaid05@gmail.com>
2025-02-07 20:49:35 -0600

    got open ai requests to work

--- main.py
+++ main.py
@@ -5,21 +5,9 @@
 # git commit -m "message"
 # git push
 
-from openai import OpenAI
-
 # client = OpenAI(
 #   api_key="sk-proj-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"
 # )
-
-# completion = client.chat.completions.create(
-#   model="gpt-4o-mini",
-# #   store=True,
-#   messages=[
-#     {"role": "user", "content": "write a haiku about ai"}
-#   ]
-# )
-
-# print(completion.choices[0].message['content'])
 from openai import OpenAI
 client = OpenAI()
 
