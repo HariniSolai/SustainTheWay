ec439f1f7f2ea316c0b16e3258bd835554656806
Kahnishga <krishnakani19@gmail.com>
2025-02-07 20:39:33 -0600

    fixed comments

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-// git pull
-// git status
-// git add .
-// git commit -m "message"
-// git push
+# git pull
+# git status
+# git add .
+# git commit -m "message"
+# git push
 
 from openai import OpenAI
 
